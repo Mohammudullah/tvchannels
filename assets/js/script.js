67001c2c957a1e61590235a91c6bf017d9ae7218
mohammudullah <mdmohammudullaha@gmail.com>
2022-11-20 16:38:00 +0600

    first

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$(window).on('load', loadPlayer('http://103.161.71.202:8080/live/Restream/allrestream/9.m3u8'));
+
+$(document).on('click', '.channel-item', function() {
+    loadPlayer($(this).attr('data-channel'));
+});
+
+
+function loadPlayer(url) {
+  const source = url;
+  const video = document.querySelector('video');
+
+  // For more options see: https://github.com/sampotts/plyr/#options
+  // captions.update is required for captions to work with hls.js
+  const player = new Plyr(video, { captions: { active: true, update: true, language: 'en' } });
+
+  if (!Hls.isSupported()) {
+    video.src = source;
+  } else {
+    // For more Hls.js options, see https://github.com/dailymotion/hls.js
+    const hls = new Hls();
+    hls.loadSource(source);
+    hls.attachMedia(video);
+    window.hls = hls;
+
+    // Handle changing captions
+    player.on('languagechange', () => {
+      // Caption support is still flaky. See: https://github.com/sampotts/plyr/issues/994
+      setTimeout(() => hls.subtitleTrack = player.currentTrack, 50);
+    });
+  }
+
+  // Expose player so it can be used from the console
+  window.player = player;
+}
